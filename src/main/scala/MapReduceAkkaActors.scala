dd1710d7a7f900642251fa0aa4d56aa84b4bc325
villaret <mateu.villaret@udg.edu>
2020-10-27 12:57:05 +0100

    MapReduce Abstracte i AKKA

--- src/main/scala/MapReduceAkkaActors.scala
+++ src/main/scala/MapReduceAkkaActors.scala
@@ -5,92 +5,131 @@ import akka.actor
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
 
-// no caldrà... serà l'actor.
+// Les case class hauran de polimòrfiques en les "claus" i "valors" que s'utilitzen
+// ja que  el MapReduce també serà polimòrfic, sinó, perdríem la genericitat.
 case class toMapper[K1,V1](fitxer: K1, text: List[V1])
 case class fromMapper[K2,V2](intermig: List[(K2,V2)])
 case class toReducer[K2,V2](word:K2, fitxers:List[V2])
 case class fromReducer[K2,V3](finals: (K2,V3))
 
 
-
+// Els actors mappers són polimòrfics ja que  reben la funció de mapping polimòrfica que han d'aplicar
 class Mapper[K1,V1,K2,V2](mapping:((K1,List[V1])) => List[(K2,V2)]) extends Actor {
   def receive: Receive = {
         // cal anotar clau:K1 i valor:List[V1] per tal d'instanciar adequadament el missatge toMapper amb les variables de tipus de Mapper
+        // Compte, que no us enganyi que hagi donat els mateixos noms a les variables de tipus al Mapper que a les case class de fora. S'han
+        // de vincular d'alguna manera perquè sinó l'inferidor de tipus no sap a quin dels paràmetres de tipus del mapper correspon el tipus de la clau
+        // i el tipus del valor al missatge toMapper
     case toMapper(clau:K1,valor:List[V1])=>
       sender ! fromMapper(mapping((clau,valor)))
-      println("Work Done by Mapper")
+      // xivato per seguir l'evolució
+      // println("Work Done by Mapper")
   }
 }
 
+// Els actors reducers són polimòrfics ja que reben la funció de reducing polimòrfica que han d'aplicar
 class Reducer[K2,V2,V3](reducing:((K2,List[V2]))=> (K2,V3)) extends Actor {
   def receive: Receive = {
-    case toReducer(w:K2,lf:List[V2])=>
-      sender ! fromReducer(reducing(w, lf))
-      println("Work Done by Reducer")
+        // cal anotar també la clau i el valor com hem fet amb els mappers
+    case toReducer(clau:K2,valor:List[V2])=>
+      sender ! fromReducer(reducing(clau, valor))
+      // println("Work Done by Reducer")
   }
 }
 
+
+
+// l'Actor MapReduce és polimòrfic amb els tipus de les claus valor de l'entrada [K1,V1], la clau i valor intermitjos [k2,v2]
+// i la clau i valor finals [K2,V3].
+// input és el paràmetre d'entrada (compte perquè depedent de la mida pot ser un problema)
+// mapping és la funció dels mappers
+// reducing és la funció dels reducers
 class MapReduce[K1,V1,K2,V2,V3](
                                  input:List[(K1,List[V1])],
                                  mapping:((K1,List[V1])) => List[(K2,V2)],
-                                 reducing:((K2,List[V2]))=> (K2,V3),
-                                  nm: Int,
-                                  nr: Int) extends Actor {
+                                 reducing:((K2,List[V2]))=> (K2,V3)) extends Actor {
 
 
 
 
   var nmappers = 0 // adaptar per poder tenir menys mappers
   var mappersPendents = 0
-  var reducersPendents = 0
   var nreducers = 0 // adaptar per poder tenir menys reducers
-  var nfiles = 0
+  var reducersPendents = 0
+  var nchunks = 0
   var num_files_mapper = 0
+  // dict serà el diccionari amb el resultat intermedi
   var dict = Map[K2, List[V2]]() withDefault (k => List())
+  // resultatFinal recollirà les respostes finals dels reducers
   var resultatFinal = Map[K2, V3]()
 
 
-
-  nfiles = input.length
-  nmappers = nfiles
-  // nmappers = nm
-  nreducers = nr
+  // farem un mapper per parella (K1,List[V1]) de l'input
+  nchunks = input.length
+  nmappers = nchunks
 
   println("Going to create MAPPERS!!")
 
+  // Al crear actors a dins d'altres actors, enlloc de crear el ActorSystem utilitzarem context i així es va
+  // organitzant la jerarquia d'actors.
+  // D'altra banda, quan els actors que creen tenen un contructor amb paràmetre, no passem el "tipus" de l'actor i prou
+  // a Props sino que creem l'actor amb els paràmetres que necessita. En aquest cas, l'Actor mapping és paramètric en tipus
+  // i necessita com a paràmetre una funció de mapping.
   val mappers = for (i <- 0 until nmappers) yield
     context.actorOf(Props(new Mapper[K1,V1,K2,V2](mapping)), "mapper"+i)
 
+  // posar les anotacions de tipus aquí no és necessari però ajuda a llegir
+  // a cada mapper li enviem una clau de tipus K1 i una llista de valors de tipus V1
   for(i<- 0 until nmappers) mappers(i) ! toMapper(input(i)._1:K1, input(i)._2: List[V1])
+  // for(i<- 0 until nmappers) mappers(i) ! toMapper(input(i)._1, input(i)._2)
+
+  // necessitem controlar quant s'han acabat tots els mappers per poder llençar els reducers després...
   mappersPendents = nmappers
 
-  println("All sent to Mappers")
+  println("All sent to Mappers, now start listening...")
+
 
   def receive: Receive = {
 
+        // Anem rebent les respostes dels mappers i les agrupem al diccionari per clau.
+        // De nou ens cal etiquetar el tipus de la clau i el valor, sinó no es pot inferir... però ara ho fem
+        // en el paràmetre del missatge directament. (comentat al for més envall, la versió alternativa).
 
-    case fromMapper(list_string_file) =>
-      for ((word:K2, file:V2) <- list_string_file)
-        dict += (word -> (file :: dict(word)))
+    case fromMapper(list_clau_valor:List[(K2,V2)]) =>
+      //for ((word:K2, file:V2) <- list_string_file)
+        for ((clau, valor) <- list_clau_valor)
+          dict += (clau -> (valor :: dict(clau)))
+      // Ja falta un mapper menys...
       mappersPendents -= 1
 
+      // quan ja hem rebut tots els missatges dels mappers:
       if (mappersPendents==0)
         {
+          // creem els reducers, tants com entrades al diccionari; fixeu-vos de nou que fem servir context i fem el new
+          // pel constructor del Reducer amb paràmetres
           nreducers = dict.size
           reducersPendents = nreducers
           val reducers = for (i <- 0 until nreducers) yield
             context.actorOf(Props(new Reducer[K2,V2,V3](reducing)), "reducer"+i)
-          for ((i,(key, value)) <-  (0 to nreducers-1) zip dict)
-            reducers(i) ! toReducer(key, value)
+
+          // ara enviem a cada reducer una clau de tipus V2 i una llista de valors de tipus K2. Les anotacions de tipus
+          // no calen
+          for ((i,(key:K2, lvalue:List[V2])) <-  (0 to nreducers-1) zip dict)
+            reducers(i) ! toReducer(key, lvalue)
           println("All sent to Reducers")
         }
 
+      // a mesura que anem rebent respostes del reducer (tuples K2, V3) les anem afegint al Map del resultatfinal i
+      // descomptem reducers pendents
     case fromReducer(entradaDictionari:(K2,V3)) =>
       resultatFinal += (entradaDictionari._1 -> entradaDictionari._2 )
       reducersPendents -= 1
+
+      // En arribar a 0, mostrem el resultat
       if (reducersPendents == 0) {
+        println("All Done from Reducers! Showing RESULTS!!!")
         for ((s,lf)<- resultatFinal) println(s+" -> " + lf)
-        println("All Done from Reducers!")
+
       }
   }
 
@@ -135,8 +174,11 @@ object Main extends App {
     }
 
   val systema = ActorSystem("sistema")
+println("Llencem l'index invertit")
+ val indexinvertit = systema.actorOf(Props(new MapReduce[File,String,String,File,Set[File]](fitxers,mappingInvInd,reducingInvInd)), name = "masterinv")
 
- // val indexinvertit = systema.actorOf(Props(new MapReduce[File,String,String,File,Set[File]](fitxers,mappingInvInd,reducingInvInd,10,10 )), name = "master")
+
+
 
   def mappingWC(tupla:(File, List[String])) :List[(String, Int)] =
     tupla match {
@@ -149,14 +191,13 @@ object Main extends App {
       case (word, nums) => (word, nums.sum)
     }
 
-
-  val wordcount = systema.actorOf(Props(new MapReduce[File,String,String,Int,Int](fitxers,mappingWC,reducingWC,10,10 )), name = "master")
-
-
+  println("Llencem el wordCount")
+  val wordcount = systema.actorOf(Props(new MapReduce[File,String,String,Int,Int](fitxers,mappingWC,reducingWC )), name = "mastercount")
 
 
 
-  // master ! IndexInvertit(nmappers,nreducers, fitxers)
+
+
 
 
 
