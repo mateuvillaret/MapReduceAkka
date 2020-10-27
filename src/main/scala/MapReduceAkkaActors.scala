80157b8309e5c415769833f3007e0fab280e78a9
villaret <mateu.villaret@udg.edu>
2020-10-27 13:46:37 +0100

    MapReduce Abstracte i AKKA

--- src/main/scala/MapReduceAkkaActors.scala
+++ src/main/scala/MapReduceAkkaActors.scala
@@ -162,6 +162,17 @@ object Main extends App {
     (f7, List("hola", "no", "pas", "adeu")),
     (f8, List("ahh", "molt", "be", "adeu")))
 
+
+  val compres: List[(String,List[(String,Double, String)])] = List(
+    ("bonpeu",List(("pep", 10.5, "1/09/20"), ("pep", 13.5, "2/09/20"), ("joan", 30.3, "2/09/20"), ("marti", 1.5, "2/09/20"), ("pep", 10.5, "3/09/20"))),
+    ("sordi", List(("pep", 13.5, "4/09/20"), ("joan", 30.3, "3/09/20"), ("marti", 1.5, "1/09/20"), ("pep", 7.1, "5/09/20"), ("pep", 11.9, "6/09/20"))),
+    ("canbravo", List(("joan", 40.4, "5/09/20"), ("marti", 100.5, "5/09/20"), ("pep", 10.5, "7/09/20"), ("pep", 13.5, "8/09/20"), ("joan", 30.3, "7/09/20"), ("marti", 1.5, "6/09/20"))),
+    ("maldi", List(("pepa", 10.5, "3/09/20"), ("pepa", 13.5, "4/09/20"), ("joan", 30.3, "8/09/20"), ("marti", 0.5, "8/09/20"), ("pep", 72.1, "9/09/20"), ("mateu", 9.9, "4/09/20"), ("mateu", 40.4, "5/09/20"), ("mateu", 100.5, "6/09/20")))
+  )
+
+
+
+
   def mappingInvInd(tupla:(File, List[String])) :List[(String, File)] =
     tupla match {
       case (file, words) =>
@@ -193,11 +204,6 @@ println("Llencem l'index invertit")
 
   println("Llencem el wordCount")
   val wordcount = systema.actorOf(Props(new MapReduce[File,String,String,Int,Int](fitxers,mappingWC,reducingWC )), name = "mastercount")
-
-
-
-
-
 
 
 
